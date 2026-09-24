96292a5b64d4f6e51e1e6ee73cdfc9abd5ced65b
gabe-gregory <ggregory@uoregon.edu>
2026-09-24 16:22:38 -0700

    update imports

--- raman_simulation/simulation.py
+++ raman_simulation/simulation.py
@@ -5,11 +5,11 @@ from scipy.signal import find_peaks
 from tqdm.notebook import tqdm
 import sympy as sp
 from scipy.signal import find_peaks
-# import useful_funcs
+import useful_funcs
 import matplotlib.pyplot as plt
 
 from .PhotonScatteringFuncs import *
-from . import useful_funcs
+# from . import useful_funcs
 
 class sim:
     def __init__(self,
